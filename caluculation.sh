b64c832ec6ebf77c703df7249b0845536c2b757f
saitheja <saithejas27@gmail.com>
2026-03-22 09:23:35 +0530

    git cherry-pick 1

--- caluculation.sh
+++ caluculation.sh
@@ -6,4 +6,11 @@ x = a+b
 # second commit 
 x = a 
 
+<<<<<<< HEAD
 # git merging by types git-merge through cherry-pick
+=======
+# division
+
+# trying to merge divison branch work using cherry-pick
+
+# merge by cherry-pick 1  >>>>>>> 57ca0f5 ( git cherry-pick 1)
